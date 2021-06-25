ab2a96630d650d5e7136d43bdb015a96ba05cfdf
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2021-06-25 11:01:39 -0700

    feat(*): convert to an oclif command

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
-#!/usr/bin/env node
-
+import { Command, flags } from '@oclif/command';
 import { command } from 'execa';
 import { globalPackageJSON } from './deps';
 import { createTempLocations, saveMinimalPackageJSON } from './fs';
 
-async function main() {
-  try {
-    const temps = await createTempLocations();
-    const packageJSON = await globalPackageJSON('npm');
-    await saveMinimalPackageJSON(temps.packageJSON, packageJSON);
-    await command('npx yarn audit -y', { stdio: 'inherit', cwd: temps.tmpDir });
-  } catch (error) {
-    console.error(
-      `A module audit found issues. The error level mask was [${error?.exitCode}] - See https://classic.yarnpkg.com/en/docs/cli/audit/#toc-yarn-audit for more.`
-    );
+class Audit extends Command {
+  static description = 'perform an audit of globally installed node_modules';
+
+  static flags = {
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+  };
+
+  static args = [];
+
+  async run() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { args, flags } = this.parse(Audit);
+
+    try {
+      const temps = await createTempLocations();
+      const packageJSON = await globalPackageJSON('npm');
+      await saveMinimalPackageJSON(temps.packageJSON, packageJSON);
+      await command('npx yarn audit -y', { stdio: 'inherit', cwd: temps.tmpDir });
+    } catch (error) {
+      this.log(
+        `A module audit found issues. The error level mask was [${error?.exitCode}] - See https://classic.yarnpkg.com/en/docs/cli/audit/#toc-yarn-audit for more.`
+      );
+
+      this.exit(1);
+    }
   }
 }
 
-main().catch((e) => console.error('An unexpected error occurred while attempting an audit of global node modules.', e));
+export = Audit;
